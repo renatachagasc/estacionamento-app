5372329a25aa3cb39d2aaf32e7bfbbe6060483a7
Renata Chagas <rehcardosochagas@gmail.com>
2021-03-21 00:31:45 -0300

    Adicionar requisição de consulta de historico

--- src/pages/historico/index.js
+++ src/pages/historico/index.js
@@ -1,44 +1,26 @@
+
 import React from 'react';
-
-import '../../App.css';
-import { Container, Row, Col, Button, Tab, Tabs, Form } from 'react-bootstrap';
+import axios from 'axios';
 
 
-function Historico(){
-    return(
-        <div>
-        <Container fluid>
-             <Row className="justify-content-center">
-                 <Col lg="4">
-                     <Tabs defaultActiveKey="entrada" id="uncontrolled-tab-example" className="justify-content-around">
-                         <Tab eventKey="entrada" title="Entrada">
-                             <Form id="entrada">
-                                 <Form.Group controlId="formValue">
-                                     <Form.Label>Número da placa:</Form.Label>
-                                     <Form.Control type="text" placeholder="AAA-0000"  />
-                                 </Form.Group>
-                                 <Button type="submit">
-                                     CONFIRMAR ENTRADA
-                                 </Button>
-                             </Form>
-                         </Tab>
-                         <Tab eventKey="saida" title="Saída">
-                             <Form id="saida">
-                                 <Form.Group controlId="formBasicEmail">
-                                 <Form.Label>Número da placa:</Form.Label>
-                                     <Form.Control type="text" placeholder="AAA-0000" />
-                                 </Form.Group>
-                                 <Button type="submit">
-                                     PAGAMENTO
-                                 </Button>
-                             </Form>
-                         </Tab>
-                     </Tabs>
-                 </Col>
-             </Row>
-         </Container>
-     </div>
-    );
-};
+export default class Historico extends React.Component {
+  state = {
+    HistoricoPlacas: []
+  }
 
-export default Historico;
+  consultarHistorico() {
+    axios.get(`https://parking-lot-to-pfz.herokuapp.com/parking/AaA-4444`)
+      .then(res => {
+        const HistoricoPlacas = res.data;
+        this.setState({ HistoricoPlacas });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.HistoricoPlacas.map(datePlate => <li>{datePlate.plate}</li>)}
+      </ul>
+    )
+  }
+}
